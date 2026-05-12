fef5b7a24fa50e1b0175296828e9f0ec1825d4f3
vishalsinha2004 <vishalsinha6567@gmail.com>
2026-05-12 23:57:43 +0530

    fix home

--- coursework/views.py
+++ coursework/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny # <-- 1. Import AllowAny
 from .models import Semester
 from .serializers import SemesterSerializer
 
 # Use ReadOnlyModelViewSet so users can fetch but NOT create/update/delete
 class SemesterViewSet(viewsets.ReadOnlyModelViewSet):
-    permission_classes = [IsAuthenticated]
+    # 2. Change permission to AllowAny so guests can see the folders on the home page
+    permission_classes = [AllowAny] 
+    
     queryset = Semester.objects.all().order_by('name')
     serializer_class = SemesterSerializer
